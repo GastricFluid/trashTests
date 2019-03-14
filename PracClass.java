df2369a7ce4e49aec563987a02cd9a6ec1ba8c09
GastricFluid <comeaupaul98@gmail.com>
2019-03-13 22:30:53 -0600

    some bad changes

--- PracClass.java
+++ PracClass.java
@@ -2,23 +2,23 @@
 public class PracClass {
 	public void feedback(int score) {
 <<<<<<< HEAD
-		if (score == 300)
+		if (score == 200)
 			System.out.println("You're p baddy");
-		else if (score > 200)
+		else if (score > 37)
 			System.out.println("That's Horrible");
 =======
-		if (score == 100)
+		if (score == 50)
 			System.out.println("You're awesome");
 		else if (score > 20)
 			System.out.println("That's great");
 >>>>>>> homerprac
-		else if (score > 10)
+		else if (score > 12)
 			System.out.println("That's Bad");
 		else
 			System.out.println("Well, what can I say?");
 	}
 	public static void main(String[] args) {
 		PracClass tm = new PracClass();
-		tm.feedback(60);
+		tm.feedback(79);
 	}
 }
