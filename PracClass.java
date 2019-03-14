39c994e59812eafca24c15242d01bc4b9e13ea8a
GastricFluid <comeaupaul98@gmail.com>
2019-03-13 22:34:03 -0600

    another conflict

--- PracClass.java
+++ PracClass.java
@@ -7,12 +7,12 @@ public class PracClass {
 		else if (score > 37)
 			System.out.println("That's Horrible");
 =======
-		if (score == 50)
+		if (score == 24)
 			System.out.println("You're awesome");
 		else if (score > 20)
 			System.out.println("That's great");
 >>>>>>> homerprac
-		else if (score > 12)
+		else if (score > 30)
 			System.out.println("That's Bad");
 		else
 			System.out.println("Well, what can I say?");
